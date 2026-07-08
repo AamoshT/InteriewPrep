7d31a1ecc43133a84bb8fb911a42a4656bd1412a
Aamosh Bikram Thapa <aamoshthapa06@gmail.com>
2026-07-07 19:06:53 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        if len(nums)==0:
+            return False
+        for i in range (len(nums)):
+            
+            if i == (len(nums)-1):
+                return False
+            if nums[i] == nums[i+1]:
+                return True
